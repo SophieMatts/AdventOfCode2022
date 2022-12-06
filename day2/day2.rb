18ffdfe58d4e8b0b3fdf36983d5f4d77be067a04
sophie mattacks <s.mattacks@hotmail.co.uk>
2022-12-06 10:08:21 +0000

    Day 1 Complete

---
+++ day2/day2.rb
@@ -0,0 +1,46 @@
+class Day2
+
+  def initialize(filename)
+    @lines = File.readlines(filename)
+  end
+
+  def part1
+
+    my_score = 0
+
+    # rock, paper, scissors
+
+    @lines.each do |line|
+      if line.include?('X') # you are rock
+        my_score += 1
+        if line.include?('A') # they are rock
+          my_score += 3
+        elsif line.include?('B') # they are paper
+          my_score += 0
+        else line.include?('C') # they are scissors
+          my_score += 6
+        end
+      elsif line.include?('Y') # you are paper
+        my_score += 2
+        if line.include?('A') # they are rock
+          my_score += 6
+        elsif line.include?('B') # they are paper
+          my_score += 3
+        else line.include?('C') # they are scissors
+          my_score += 0
+        end
+      else line.include?('Z') # you are scissors
+        my_score += 3
+        if line.include?('A') # they are rock
+          my_score += 0
+        elsif line.include?('B') # they are paper
+          my_score += 6
+        else line.include?('C') # they are scissors
+          my_score += 3
+        end
+      end
+    end
+    return my_score
+
+  end
+end
---
+++ day2/day2_test.rb
@@ -0,0 +1,25 @@
+require_relative "day2"
+require "test/unit"
+
+class TestDay2 < Test::Unit::TestCase
+  def test_example_part1
+    day2 = Day2.new('day2_example.txt')
+   assert_equal(15, day2.part1())
+  end
+
+  def test_part1
+   day2 = Day2.new('day2.txt')
+   assert_equal(11841, day2.part1())
+ end
+
+  #def test_example_part2
+   # day1 = Day1.new('day1_example.txt')
+   # assert_equal(45000, day1.part2())
+ # end
+
+ # def test_part2
+   # day1 = Day1.new('day1.txt')
+  #  assert_equal(200158, day1.part2())
+ # end
+
+end
